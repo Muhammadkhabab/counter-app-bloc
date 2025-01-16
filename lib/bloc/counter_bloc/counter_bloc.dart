005970269909b5fac32b229150f4af301d716126
Query Byte Tech <khababkhattak332@gmail.com>
2025-01-16 12:24:08 +0500

    counter bloc refactor

--- lib/bloc/counter_bloc/counter_bloc.dart
+++ lib/bloc/counter_bloc/counter_bloc.dart
@@ -1,6 +1,6 @@
 import 'package:bloc/bloc.dart';
-import 'package:bloc_counter_app/bloc/counter_event.dart';
-import 'package:bloc_counter_app/bloc/counter_state.dart';
+import 'package:bloc_counter_app/bloc/counter_bloc/counter_event.dart';
+import 'package:bloc_counter_app/bloc/counter_bloc/counter_state.dart';
 
 class CounterBloc extends Bloc<CounterEvent, CounterState> {
   CounterBloc() : super(CounterState()) {
--- lib/bloc/counter_bloc/counter_state.dart
+++ lib/bloc/counter_bloc/counter_state.dart
@@ -5,6 +5,7 @@ class CounterState extends Equatable {
 
   const CounterState({this.counter = 1});
 
+  //copyWith method to create the new instance of this class
   CounterState copyWith({int? counter}) {
     return CounterState(counter: counter ?? this.counter);
   }
